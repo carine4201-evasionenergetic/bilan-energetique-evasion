caeb7cb36ef4e3a2daab9d6c513e94e8e7687489
carine4201-evasionenergetic <carine4201@gmail.com>
2026-08-02 21:44:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.querySelector('.btn').addEventListener('click',function(e){
 e.preventDefault();
 alert("Le questionnaire sera ajouté à l'étape suivante 🌿");
 });
+document.querySelectorAll(".answer").forEach(card => {
+    card.addEventListener("click", function () {
+        document.querySelectorAll(".answer").forEach(c => c.style.border = "none");
+        this.style.border = "3px solid #5c8c63";
+    });
+});
